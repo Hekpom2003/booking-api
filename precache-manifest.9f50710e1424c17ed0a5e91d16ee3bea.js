ca3dce37c8e8dee2c52687ed875ec391719fc7c9
dmitry <Hekpom2003@icloud.com>
2019-10-22 16:32:03 +0300

    Updates

---
+++ precache-manifest.9f50710e1424c17ed0a5e91d16ee3bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81c2d031881d9749ecf0387cf445326e",
+    "url": "/booking-api/index.html"
+  },
+  {
+    "revision": "f420c9a9b5223af6b57c",
+    "url": "/booking-api/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "edabf359ba100f6732f9",
+    "url": "/booking-api/static/js/2.77412952.chunk.js"
+  },
+  {
+    "revision": "f420c9a9b5223af6b57c",
+    "url": "/booking-api/static/js/main.e03fe8f4.chunk.js"
+  },
+  {
+    "revision": "4e0765c9ac9632587b85",
+    "url": "/booking-api/static/js/runtime-main.832744f5.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/booking-api/static/media/logo.25bf045c.svg"
+  }
+]);
